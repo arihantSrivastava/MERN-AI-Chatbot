8026c706c3d8361c24b61696a0b42e45388005fe
Arihant Srivastava <arihantsrivastava@Arihants-MacBook-Air.local>
2024-08-10 00:28:59 +0530

    Setup Node, Express App with TypeScript

--- backend_1/dist/index.js
+++ backend_1/dist/index.js
@@ -1,4 +1,9 @@
 import express from "express";
 const app = express();
-app.listen(5000, () => console.log("hey"));
+app.use(express.json());
+app.post('/hello', (req, res, next) => {
+    res.send("hello");
+    console.log(req.body.name);
+});
+app.listen(5000, () => console.log("Server Started"));
 //# sourceMappingURL=index.js.map
--- backend_1/src/index.ts
+++ backend_1/src/index.ts
@@ -1,5 +1,13 @@
 import express from "express"
+import { CLIENT_RENEG_WINDOW } from "tls";
 
 const app = express()
 
-app.listen(5000,()=> console.log("hey lala"))
+
+
+app.use(express.json())
+app.post('/hello' , (req,res,next)=>{
+     res.send("hello");
+    console.log(req.body.name)
+})
+app.listen(5000,()=> console.log("Server Started"))
