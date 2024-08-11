6a96748e763f7b8df8908f74622ec2380bab9b95
Arihant Srivastava <arihantsrivastava@Arihants-MacBook-Air.local>
2024-08-12 03:38:58 +0530

    user-controller functions created getallusers and signup function

--- backend_1/dist/controllers/user-controllers.js
+++ backend_1/dist/controllers/user-controllers.js
@@ -1,3 +1,27 @@
-export const getAllUsers = () => {
+import User from "../models/user.js";
+import { hash } from "bcrypt";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "All users", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "error", cause: error.message });
+    }
+};
+export const signup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        console.log(user._id);
+        return res.status(200).json({ message: "User Created", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "error", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend_1/dist/routes/user-routes.js
+++ backend_1/dist/routes/user-routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controllers.js";
+import { getAllUsers, signup } from "../controllers/user-controllers.js";
 const userRoutes = Router();
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", signup);
 export default userRoutes;
 //# sourceMappingURL=user-routes.js.map
